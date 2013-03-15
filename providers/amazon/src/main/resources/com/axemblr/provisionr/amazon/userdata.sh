6559ede8b1df7c26761183e7e3df89a879aa6672
Alex Ciminian <alex.ciminian@gmail.com>
2013-03-15 17:36:25 +0200

    PROVISIONR-22. First steps for golden AMI support:
    * The ability to create machines in a pool provisioned with a specific AMI
    * New command to support the creation of an AMI from a template
    
    Signed-off-by: Andrei Savu <asavu@apache.org>

--- providers/amazon/src/main/resources/com/axemblr/provisionr/amazon/userdata.sh
+++ providers/amazon/src/main/resources/com/axemblr/provisionr/amazon/userdata.sh
@@ -10,7 +10,7 @@ wget http://apt.puppetlabs.com/puppetlabs-release-$DISTRIB_CODENAME.deb
 dpkg -i puppetlabs-release-$DISTRIB_CODENAME.deb
 
 apt-get update
-apt-get install puppet-common
+apt-get install -y puppet-common
 
 # TODO implement a special action for global package upgrades 
 # apt-get upgrade -y
